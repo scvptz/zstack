be7e40f797879809a68147f72ca1287452e82f32
miao.DengSheng <xuexuemiao@yeah.net>
2016-10-17 17:25:27 +0800

    drop primary key of LocalStorageHostRefVO before add new one

--- conf/db/upgrade/V1.8__schema.sql
+++ conf/db/upgrade/V1.8__schema.sql
@@ -1,5 +1,6 @@
 #
-ALTER TABLE LocalStorageHostRefVO DROP FOREIGN KEY `fkLocalStorageHostRefVOHostEO`;
-ALTER TABLE LocalStorageHostRefVO DROP index `hostUuid`;
-ALTER TABLE LocalStorageHostRefVO ADD CONSTRAINT `fkLocalStorageHostRefVOHostEO` FOREIGN KEY (`hostUuid`) REFERENCES `HostEO` (`uuid`) ON DELETE CASCADE;
-ALTER TABLE LocalStorageHostRefVO ADD CONSTRAINT `pkHostUuidPrimaryStorageUuid` PRIMARY KEY (`hostUuid`,`primaryStorageUuid`);
+ALTER TABLE `LocalStorageHostRefVO` DROP FOREIGN KEY `fkLocalStorageHostRefVOHostEO`;
+ALTER TABLE `LocalStorageHostRefVO` DROP INDEX `hostUuid`;
+ALTER TABLE `LocalStorageHostRefVO` DROP PRIMARY KEY;
+ALTER TABLE `LocalStorageHostRefVO` ADD CONSTRAINT `fkLocalStorageHostRefVOHostEO` FOREIGN KEY (`hostUuid`) REFERENCES `HostEO` (`uuid`) ON DELETE CASCADE;
+ALTER TABLE `LocalStorageHostRefVO` ADD CONSTRAINT `pkHostUuidPrimaryStorageUuid` PRIMARY KEY (`hostUuid`,`primaryStorageUuid`);
