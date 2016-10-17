4699f8b8a6dec4720877544e452742635e679eda
miao.DengSheng <xuexuemiao@yeah.net>
2016-10-17 11:21:56 +0800

    add composite primary key for localStorageHostRefVO

--- conf/db/upgrade/V1.8__schema.sql
+++ conf/db/upgrade/V1.8__schema.sql
@@ -1,4 +1,5 @@
 #
 ALTER TABLE LocalStorageHostRefVO DROP FOREIGN KEY `fkLocalStorageHostRefVOHostEO`;
 ALTER TABLE LocalStorageHostRefVO DROP index `hostUuid`;
-ALTER TABLE LocalStorageHostRefVO ADD  CONSTRAINT `fkLocalStorageHostRefVOHostEO` FOREIGN KEY (`hostUuid`) REFERENCES `HostEO` (`uuid`) ON DELETE CASCADE;
+ALTER TABLE LocalStorageHostRefVO ADD CONSTRAINT `fkLocalStorageHostRefVOHostEO` FOREIGN KEY (`hostUuid`) REFERENCES `HostEO` (`uuid`) ON DELETE CASCADE;
+ALTER TABLE LocalStorageHostRefVO ADD CONSTRAINT `pkHostUuidPrimaryStorageUuid` PRIMARY KEY (`hostUuid`,`primaryStorageUuid`);
